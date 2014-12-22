7daffe845580493b4033eec89e4a3b7dd64a656d
Alez <alez2005@yandex.ru>
2014-12-22 10:51:10 +0300

    initial commit

---
+++ environments/dev/backend/config/main-local.php
@@ -0,0 +1,30 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '',
+            // unique CSRF cookie parameter for backend (set by kartik-v/yii2-app-practical-a)
+            'csrfParam' => '_backendCsrf',
+        ],
+        // unique identity cookie parameter for backend (set by kartik-v/yii2-app-practical-a)
+        'user' => [
+            'identityCookie' => [
+                'name' => '_backendUser', // unique for backend
+                'path' => '/practical-a/backend' // set it to correct path for backend app.
+            ]
+        ]
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
