ca07887483f7cbb1f3cf1897dd3cc4cc19367d6b
Alez <alez2005@yandex.ru>
2015-01-06 00:32:24 +0300

    Шаблоны для быстрой генерации админки

---
+++ common/templates/crud/minimal/views/update.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+$urlParams = $generator->generateUrlParams();
+
+echo "<?php\n";
+?>
+/* @var $this yii\web\View */
+/* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
+
+$this->title = <?= $generator->generateString('Изменить {modelClass}: ', ['modelClass' => Inflector::camel2words(StringHelper::basename($generator->modelClass))]) ?> . ' ' . $model-><?= $generator->getNameAttribute() ?>;
+$this->params['breadcrumbs'][] = ['label' => <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>, 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-update">
+    <?= "<?= " ?>$this->render('_form', [
+        'model' => $model,
+    ]) ?>
+</div>
